f7cb118c507d2a63e6efa6c6d3a795e8cae56f28
Yash Gandhi <31920628+yashrajgandhi95@users.noreply.github.com>
2022-06-24 19:54:56 -0700

    commit on main

--- test.cpp
+++ test.cpp
@@ -1,8 +1,8 @@
 #include "logic.h"
-#include "gtest/gtest.h"
 #include <stdint.h>
 #include <conio.h>
+#include <windows.h>
+#include"gtest/gtest.h"
 
-TEST(FifoNotFull,HandleFifoNotFullCondition){
-    EXPECT_EQ(logic(1,[0]), 0);
-}
+#define volatile boolean fifo_full_flag
+#define data(uint8_t[]) = {0xff, 0xff, 0xA5, 0x5A, 0x01, 0x0F, 0xA5, 0x5A, 0xff, 0xff, 0xA5, 0x5A, 0x01, 0x0F, 0xA5, 0x5A }
